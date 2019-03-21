a2af051185a7a219f6691fd190e21f0aafe80da3
Elena St <elenaperfectworld@aol.com>
2019-03-21 21:28:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your functions and code here
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name){
+ return  kittens.shift(name)
+}
+
+function appendKitten(name){
+  return kittens.concat(name)
+} 
+
+function prependKitten(name){
+  return kittens.slice(name+kittens)
+}
+
+function removeLastKitten(name){
+  return kittens.slice()
+}
